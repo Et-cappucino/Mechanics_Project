ecfab9a747b494bf394f7025af8389601388ac1e
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2022-05-17 19:19:23 +0400

    Add FT class impl and the Complex util class

---
+++ src/main/java/Complex.java
@@ -0,0 +1,34 @@
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Complex {
+
+    private double re;
+    private double im;
+
+    public Complex add(Complex b) {
+        return new Complex(this.re + b.re, this.im + b.im);
+    }
+
+    public Complex sub(Complex b) {
+        return new Complex(this.re - b.re, this.im - b.im);
+    }
+
+    public Complex mult(Complex b) {
+        return new Complex(this.re * b.re - this.im * b.im,
+                this.re * b.im + this.im * b.re);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%f, %f i)", re, im);
+    }
+}
