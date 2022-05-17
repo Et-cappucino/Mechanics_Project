091b5e5cb1d0f64ef492b3c622418dc22a23c988
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2022-05-17 18:53:12 +0400

    Add SpringArray class impl with its util methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -8,5 +10,22 @@ public class Main {
         int[] digits = {1, 1, 0, 0, 1, 1, 0, 1};
         Converter converter = new Converter();
         System.out.println(converter.validate(digits));
+
+        // For testing
+
+        String expr = "{[{}{}][]}";
+        String expr2 = "{[{[][]}{}][{}]}";
+
+        ArrayList<List<Integer>> indexPairs = SpringArray.getIndexPairs(expr);
+        System.out.println("Expression: " + expr);
+        System.out.println("Size: " + expr.length());
+        System.out.println(indexPairs);
+
+        System.out.println("------------------------------------------------------------------------------");
+
+        ArrayList<List<Integer>> indexPairs2 = SpringArray.getIndexPairs(expr2);
+        System.out.println("Expression: " + expr2);
+        System.out.println("Size: " + expr2.length());
+        System.out.println(indexPairs2);
     }
 }
